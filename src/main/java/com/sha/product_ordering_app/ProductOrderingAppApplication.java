ef84c1f5573b39a62f551fbf057417cc63807f59
irem-yigit <yigitiremm@gmail.com>
2025-08-29 02:50:33 +0300

    first commit - product-ordering-app spring boot project

---
+++ src/main/java/com/sha/product_ordering_app/ProductOrderingAppApplication.java
@@ -0,0 +1,13 @@
+package com.sha.product_ordering_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrderingAppApplication.class, args);
+	}
+
+}
