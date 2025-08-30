5744538ac70a62ab49588f6293208d29f0fd4f43
irem-yigit <yigitiremm@gmail.com>
2025-08-31 00:00:15 +0300

    feat: implement security config layer

---
+++ product-service/src/main/java/com/sha/product_service/security/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.sha.product_service.security;
+
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+
+public class SecurityConfig implements WebSecurityConfigurer {
+
+}
