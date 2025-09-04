ec464a8d1e4ef4ea4afee88c8c9687728c4be909
irem-yigit <yigitiremm@gmail.com>
2025-09-05 02:37:13 +0300

    refactor: improve security config layer

--- product-service/src/main/java/com/sha/product_service/security/SecurityConfig.java
+++ product-service/src/main/java/com/sha/product_service/security/SecurityConfig.java
@@ -1,7 +1,21 @@
 package com.sha.product_service.security;
 
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableWebSecurity
 public class SecurityConfig implements WebSecurityConfigurer {
 
+
+    @Override
+    public void init(SecurityBuilder builder) throws Exception {
+
+    }
+
+    @Override
+    public void configure(SecurityBuilder builder) throws Exception {
+
+    }
 }
